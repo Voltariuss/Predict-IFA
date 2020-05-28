5546bbdb0ce7b64abc8d49fd7bb6b00ec1204b24
Aleryc <alerycserrania@gmail.com>
2020-05-28 11:24:05 +0200

    Add: Test registration client

--- src/main/java/fr/insalyon/b05/predictifa/ihm/console/Main.java
+++ src/main/java/fr/insalyon/b05/predictifa/ihm/console/Main.java
@@ -7,26 +7,41 @@ package fr.insalyon.b05.predictifa.ihm.console;
 
 import fr.insalyon.b05.predictifa.dao.JpaUtil;
 import fr.insalyon.b05.predictifa.models.Client;
+import fr.insalyon.b05.predictifa.models.Person;
 import fr.insalyon.b05.predictifa.services.ClientService;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  *
  * @author aleryc
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         JpaUtil.init();
         
-        System.out.println("succès");
+        testRegistrationClient();
         
         JpaUtil.destroy();
     }
     
-    public static void testRegistrationClient() {
+    public static void testRegistrationClient() throws Exception {
         ClientService clientService = new ClientService();
         Client newClient = new Client(
-            
+            "Balance", 
+            "Tigre", 
+            "Bleu", 
+            "Chat", 
+            "alerycserrania@gmail.com", 
+            "toto", 
+            "Aleryc", 
+            "Serrania", 
+            "11 rue des élites 69100 Villeurbanne", 
+            new GregorianCalendar(1997, Calendar.FEBRUARY, 11).getTime(), 
+            "0712486521", 
+            Person.Gender.M
         );
-        clientService.registration(client);
+        
+        clientService.registration(newClient);
     }
 }
