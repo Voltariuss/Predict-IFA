f8fe43acf745ac593929fdcb3082668cb35a442a
Aleryc <alerycserrania@gmail.com>
2020-06-04 19:30:58 +0200

    Add: Employee entity

---
+++ src/main/java/fr/insalyon/b05/predictifa/models/Employee.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insalyon.b05.predictifa.models;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author aleryc
+ */
+@Entity
+@DiscriminatorValue("C")
+public class Employee extends Person {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Column(nullable = false)
+    private String proPhoneNumber;
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (this.getId() != null ? this.getId().hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Employee)) {
+            return false;
+        }
+        Employee other = (Employee) object;
+        if ((this.getId() == null && other.getId() != null) || (this.getId() != null && !this.getId().equals(other.getId()))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "fr.insalyon.b05.predictifa.models.Employee[ id=" + this.getId() + " ]";
+    }
+    
+}
