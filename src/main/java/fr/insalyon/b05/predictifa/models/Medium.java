7d7d1a83d6523b9b78f624ba63022c4ee4b66958
Voltariuss <loic.duboistermoz@gmail.com>
2020-06-05 15:27:09 +0200

    Add: Medium entity class

---
+++ src/main/java/fr/insalyon/b05/predictifa/models/Medium.java
@@ -0,0 +1,82 @@
+package fr.insalyon.b05.predictifa.models;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Medium implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String denomination;
+    
+    @Column(nullable = false)
+    private Gender gender;
+    
+    @Column(nullable = false)
+    private String presentation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDenomination() {
+        return denomination;
+    }
+
+    public void setDenomination(String denomination) {
+        this.denomination = denomination;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getPresentation() {
+        return presentation;
+    }
+
+    public void setPresentation(String presentation) {
+        this.presentation = presentation;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Medium)) {
+            return false;
+        }
+        Medium other = (Medium) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "fr.insalyon.b05.predictifa.models.Medium[id=" + this.id
+                + ", denomination=" + this.denomination
+                + ", gender=" + this.gender
+                + ", presentation=" + this.presentation + "]";
+    }
+}
